d4b5bf8a3c2747237636879fe4d1e962b201cf27
Jesus Hernandez <jesushernandezcp@gmail.com>
2019-08-25 17:41:18 -0500

    TERCERA COMMIT DE ACTUALIZACION PRACTICA 3 ACTIVIDAD 2 FUNCION 3

--- main.c
+++ main.c
@@ -26,9 +26,9 @@ printf("\n\t\t\t EL VALOR DE LA FUNCION 1 (x-1)/4 - (x-5)/36 ES IGUAL A = %.2f \
 }while (x<10);
 */
 
-printf("\n\t\t\t ----EVALUANDO FUNCION---- 2");
+//printf("\n\t\t\t ----EVALUANDO FUNCION---- 2");
 
-do
+/*do
 {
 //funcion 2//
 printf("\n\n");
@@ -38,6 +38,18 @@ scanf("%f",&x);
 fun2=(6*(x+1)/8)-((2*x-3)/16);
 
 printf("\n\t\t\t EL VALOR DE LA FUNCION 2 6{(x-1)/8 - (2x-3)/16} ES IGUAL A = %.2f\n",fun2);
+}while(x<10);
+*/
+
+printf("\n\t\t\t ----EVALUANDO ACTIVIDAD 2 FUNCION---- 3");
+
+do
+{
+printf("\n\n");
+printf("ASIGNE UN VALOR A LA X: ");
+scanf("%f",&x);
+fun3=(float)2/3*(x-(1-((x-2)/3)))+1;
+printf("\n\t\t\t EL VALOR DE LA FUNCION 3 6{(x-1)/8 - (2x-3)/16} ES IGUAL A = %f\n",fun3);
 }while(x<10);
 
 system("PAUSE");
